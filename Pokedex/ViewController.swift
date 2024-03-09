aabe4f0f53f2b026ae362ed360be91f83f2ebae7
Boximayo <mayo_black@outlook.com>
2024-03-08 21:11:27 -0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Mayo Gonzalez ortega on 08/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
